e6eb63366d385d6015b00bd3f953c656ef82ae3b
nitesik <neetesh2000@outlook.com>
2025-04-01 15:32:04 +0600

    updates

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -2,7 +2,7 @@ export const Images = {
   backgroundImg:
     "https://images.unsplash.com/photo-1566020586758-23d5e0ff7a64?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   imageOne:
-    "https://res.cloudinary.com/dqlrcrire/image/upload/v1738640481/IMG_0207_xcq7vy.jpg",
+    "https://res.cloudinary.com/dqlrcrire/image/upload/v1743499196/cc393489-4032-4c52-83a8-9f56f4e19c75_hhijpa.jpg",
   imageTwo:
     "https://res.cloudinary.com/dqlrcrire/image/upload/v1738559291/IMG_0041_esew3a.jpg",
   imageThree:
@@ -11,6 +11,14 @@ export const Images = {
     "https://res.cloudinary.com/dqlrcrire/image/upload/v1738559299/IMG_0081_dd6ogf.jpg",
   imageFive:
     "https://res.cloudinary.com/dqlrcrire/image/upload/v1738559303/IMG_0151-3_y9lacc.jpg",
+  imageSix:
+    "https://res.cloudinary.com/dqlrcrire/image/upload/v1743499341/7687e097-3dc2-427b-b2d3-c592c168a07a_pvoar5.jpg",
+  imageSeven:
+    "https://res.cloudinary.com/dqlrcrire/image/upload/v1743499338/1c50aeb8-748f-446d-ad4b-520121354191_p9i8yn.jpg",
+  imageEight:
+    "https://res.cloudinary.com/dqlrcrire/image/upload/v1743499326/71bc456a-8ed3-4fff-b84b-e6a572bf0bcb_ae2oxg.jpg",
+  imageNine:
+    "https://res.cloudinary.com/dqlrcrire/image/upload/v1743499334/83d7b0f6-01e3-4948-ab1a-f88090628cb5_keprpv.jpg",
 };
 
 export const CarouselImages = [
